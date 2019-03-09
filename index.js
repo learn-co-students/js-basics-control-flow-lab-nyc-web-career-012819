ede50fc2dd4bfcc2e1983240fb9dbf814285fcc4
github username <github email address>
2019-03-09 15:47:04 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // Write your code in this file!
+function scuberGreetingForFeet(rideft) {
+  let message;
+  if (rideft <= 400) {
+    message = 'This one is on me!'
+  }
+  else if (rideft > 2000 && rideft < 2500) {
+    message = 'I will gladly take your thirty bucks.'
+  }
+  else if (rideft > 2500) {
+    message = 'No can do.'
+  }
+  return message
+}
+
+function ternaryCheckCity(city) {
+  let mes;
+  if (city === 'NYC') {
+    mes = "Ok, sounds good."
+  } else {
+    mes = "No go."
+  }
+  return mes
+}
+
+function switchOnCharmFromTip(tip) {
+  let mmm;
+  switch (tip) {
+    case 'generous':
+      mmm = "Thank you so much.";
+      break;
+    case 'not as generous':
+    mmm = "Thank you.";
+    break;
+    default:
+      mmm = 'Bye.'
+  }
+  return mmm
+}
