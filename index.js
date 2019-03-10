41228075eb188fedc64dda289c434e42f7a7d865
stomg7969 <stomg7969@gmail.com>
2019-03-09 19:34:18 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Write your code in this file!
+function scuberGreetingForFeet(number) {
+  if (number <= 400) {
+    return 'This one is on me!';
+
+  } else if (number > 2000 && number < 2499) {
+    return 'I will gladly take your thirty bucks.';
+
+  } else if (number > 2500) {
+    return 'No can do.';
+  };
+};
+
+function ternaryCheckCity(city) {
+  return (city === 'NYC' ? 'Ok, sounds good.' : 'No go.');
+};
+
+function switchOnCharmFromTip(tip) {
+  switch (tip) {
+    case 'generous':
+      return 'Thank you so much.';
+      break;
+    case 'not as generous':
+      return 'Thank you.';
+      break;
+    default:
+      return 'Bye.';
+  };
+};
