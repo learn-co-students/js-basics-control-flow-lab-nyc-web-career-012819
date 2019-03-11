9d399c2f234e9cf7cdf516248db48dfdb2509091
faizaniq <github email address>
2019-03-11 00:02:56 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Write your code in this file!
+function scuberGreetingForFeet(ride)
+{
+  let result
+  if (ride <= 400)
+  {
+    result = "This one is on me!"
+  } else if (ride <= 2500 )
+  {
+    result = "I will gladly take your thirty bucks."
+  } else if (ride > 2500 )
+  {
+    result = "No can do."
+  }
+  return result
+}
+
+function ternaryCheckCity(city)
+{
+  return city === 'NYC' ? 'Ok, sounds good.' : 'No go.';
+}
+
+function switchOnCharmFromTip(tip)
+{
+  switch (tip) {
+    case 'generous':
+      return 'Thank you so much.';
+    case 'not as generous':
+      return 'Thank you.';
+    default:
+      return 'Bye.';
+  }
+}
